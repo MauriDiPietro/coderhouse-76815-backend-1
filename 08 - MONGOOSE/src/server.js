8a7f9e347422a31c1dd9502c03f381be9d37ce6d
MauriDiPietro <dipietro.jm@gmail.com>
2026-03-17 21:15:09 -0300

    clase08

--- 08 - MONGOOSE/src/server.js
+++ 08 - MONGOOSE/src/server.js
@@ -1,8 +1,19 @@
-import express from 'express';
+import express from "express";
+import { connectMongoDB } from "./config/db-connection.js";
 // import 'dotenv/config'
+import productRouter from './routes/product-router.js'
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/products', productRouter)
+
 const PORT = process.env.PORT || 3000;
+
+connectMongoDB()
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((error) => console.log(error));
 
 app.listen(PORT, () => console.log(`Server ok on port ${PORT}`));
